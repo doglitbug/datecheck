d98be4037a17e535a6856e8bd94e5f0fad7986bb
Arron Dick <s_drac2@yahoo.com>
2018-08-27 23:51:09 +0000

    Added Categories item and database seeder

--- database/migrations/2018_08_27_234009_create_categories_table.php
+++ database/migrations/2018_08_27_234009_create_categories_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateItemsTable extends Migration
+class CreateCategoriesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,9 @@ class CreateItemsTable extends Migration
      */
     public function up()
     {
-        Schema::create('items', function (Blueprint $table) {
+        Schema::create('categories', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name')->nullable(false);
-            $table->string('barcode');
         });
     }
 
@@ -27,6 +26,6 @@ class CreateItemsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('items');
+        Schema::dropIfExists('categories');
     }
 }
---
+++ database/seeds/CategoryTableSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class CategoryTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+    	
+        //Add a view default categories for testing
+        //Not using a faker class, so that we have real items and barcodes for later testing
+    	DB::table('categories')->insert([
+    		[
+    			'name' => 'Drinks',
+    		],
+    		[
+    			'name' => 'Snacks',
+    		],
+    		[
+    			'name'=>'Breakfast',
+    		]
+    	]);
+    }
+}

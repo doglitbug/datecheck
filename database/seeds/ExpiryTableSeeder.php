3b3db822c3539255743a9695cadf6e38bba5f767
Arron Dick <s_drac2@yahoo.com>
2018-10-30 05:34:04 +0000

    Randomized some dates in seeder

--- database/seeds/ExpiryTableSeeder.php
+++ database/seeds/ExpiryTableSeeder.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Seeder;
 use App\Expiry;
+use Carbon\Carbon;
 
 class ExpiryTableSeeder extends Seeder
 {
@@ -14,7 +15,13 @@ class ExpiryTableSeeder extends Seeder
     {
         //Grab all items
         foreach(App\Item::all() as $item){
-        	$item->expiry()->create(['expiry_date'=>'2018-12-30']);
+            //Add random number of dates to item
+            for($i=1;$i<rand(1,4);$i++){
+                //Choose a date between a week ago and 3 weeks in future
+                $randomDate = Carbon::now()->addDays(rand(-7, 21))->format('Y-m-d');
+
+            	$item->expiry()->create(['expiry_date'=>$randomDate]);
+            }
         }
     }
 }

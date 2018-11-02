87a2714bf2fa0662ca3298225489c1895b0ba735
Arron Dick <s_drac2@yahoo.com>
2018-11-02 05:32:29 +0000

    Tidied up show item HTML

--- resources/views/items/show.blade.php
+++ resources/views/items/show.blade.php
@@ -4,54 +4,56 @@
 
 @section('content')
 
-<div class="panel-body">
-	<h1>{{ $item->name }}</h1>
-	<ul>
-		<li>Barcode: {{ $item->barcode }}</li>
-		<li>Category: {{ $item->category->name }}</li>
-		<li>Expiry dates:
-			<ul>
+<div class="panel panel-primary">
+	<div class="panel-heading">
+		<h3 class="panel-title">{{ $item->name }}</h3>
+	</div>
+	<div class="panel-body">
+		<ul class="list-group">
+			<li class="list-group-item">Barcode: {{ $item->barcode }}</li>
+			<li class="list-group-item">Category: {{ $item->category->name }}</li>
+			<li class="list-group-item">Expiry dates:
+				<ul>
 				@foreach($item->expiry->sortBy('expiry_date') as $expiry)
-				<li>
-					<!-- Delete button -->
-					<form action="/expiry/{{ $expiry->id }}" method="POST">
-						{{ csrf_field() }}
-						{{ method_field('DELETE') }}
-						{{ $expiry->expiry_date }}
-        				<button type="submit" class="btn btn-danger">
-							<i class="fa fa-btn fa-trash"></i></button>
-    				</form>
-				</li>
+					<li>
+						<!-- Delete button -->
+						<form action="/expiry/{{ $expiry->id }}" method="POST">
+							{{ csrf_field() }}
+							{{ method_field('DELETE') }}
+							{{ $expiry->expiry_date }}
+							<button type="submit" class="btn btn-danger">
+								<i class="fa fa-btn fa-trash"></i></button>
+    					</form>
+					</li>
 				@endforeach
-			</ul>
-			<li>
-				<!-- Add new date -->
-				<form action="/expiry/{{ $item->id }}" method="POST">
-					{{ csrf_field() }}
-					<div class="form-group">
-						<!-- Expiry date -->
-						<label for="expiry-date" class="col-sm-3 control-label">New date</label>
-						<div class="col-sm-6">
-							<input type="text" name="expiry_date" id="expiry-date" class="form-control">
-						</div>
-					</div>
-					<div class="form-group">
-						<div class="col-sm-offset-3 col-sm-6">
-							<button type="submit" class="btn btn-default">
-								<i class="fa fa-plus"></i>Add Date
-							</button>
-						</div>
-					</div>
-				</form>
+				</ul>
 			</li>
-	</ul>
-	<!-- Delete button -->
-	<form action="/items/{{ $item->id }}" method="POST">
-		{{ csrf_field() }}
-		{{ method_field('DELETE') }}
+			<li class="list-group-item">
+			<!-- Add new date -->
+			<form action="/expiry/{{ $item->id }}" method="POST">
+				{{ csrf_field() }}
+				<div class="form-group">
+					<!-- Expiry date -->
+					<label for="expiry-date" class="control-label">New date</label>
+					<input type="text" name="expiry_date" id="expiry-date" class="form-control">
+				</div>
+				<div class="form-group">
+					<button type="submit" class="btn btn-default">
+						<i class="fa fa-plus"></i>Add Date
+					</button>
+				</div>
+			</form>
+			</li>
+		</ul>
+	</div>
+	<div class="panel-footer">
+		<!-- Delete button -->
+		<form action="/items/{{ $item->id }}" method="POST">
+			{{ csrf_field() }}
+			{{ method_field('DELETE') }}
 
-        <button type="submit" class="btn btn-danger">
-			<i class="fa fa-btn fa-trash"></i>Delete</button>
-    </form>
+			<button type="submit" class="btn btn-danger"><i class="fa fa-btn fa-trash"></i></button>
+	    </form>
+	</div>
 </div>
 @endsection

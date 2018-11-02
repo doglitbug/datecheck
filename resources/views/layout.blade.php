9823be56e7497aecf9d3d5bd73ca661d6e188e6e
Arron Dick <s_drac2@yahoo.com>
2018-11-02 06:13:53 +0000

    Clean up of HTML

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -17,14 +17,12 @@
 
 	<body>
 		<div class="container">
-			
-			@include('navbar')
-		</div>
+		@include('navbar')
 		<!-- Display Validation Errors -->
         @include('errors')
         
 		@yield('content')
-
+		</div>
 		<!-- JavaScripts -->
     	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
     	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>

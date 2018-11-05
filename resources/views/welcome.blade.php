2b8b180d7e1eb0d9ce881ff8466e15ee3716b271
Arron Dick <s_drac2@yahoo.com>
2018-11-05 08:34:49 +0000

    Added stats on main page for expired/un-expired items

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,6 +13,8 @@
                 <li class="list-group-item">Total Items: {{ App\Item::all()->count() }}</li>
                 <li class="list-group-item">Total Categories: {{ App\Category::all()->count() }}</li>
                 <li class="list-group-item">Total Expiry Dates recorded: {{ App\Expiry::all()->count() }}</li>
+                <li class="list-group-item">Expired items: {{ App\Expiry::whereDate('expiry_date', '<=', Carbon\Carbon::now())->get()->count() }}</li>
+                <li class="list-group-item">Un expired items: {{ App\Expiry::whereDate('expiry_date', '>', Carbon\Carbon::now())->get()->count() }}</li>
             </ul>
         </div>
     </div>

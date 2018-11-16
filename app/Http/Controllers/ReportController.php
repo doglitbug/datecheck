00e7d0f2039dd47ad246d6c32d11baebf75f6cfb
Arron Dick <s_drac2@yahoo.com>
2018-11-16 01:27:05 +0000

    Revert "Added extra report"
    This reverts commit 08b0095652b97a382b7217e4b4b194e608eeacf8.

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -26,31 +26,18 @@ class ReportController extends Controller
 		//Include today
 		$end   = Carbon::now();
 
-		$items = $this->getItems($start, $end);
+		//Grab required items within those dates
+		$items = Item::whereHas('expiry', function ($query) {
+					$query->whereDate('expiry_date', '<=', Carbon::now());
+					})->with(['expiry' => function ($query) {
+					$query->whereDate('expiry_date', '<=', Carbon::now())->orderBy('expiry_date');
+					}])->get();
 
 		return view('reports.expired', array('pageTitle'=>"Expired items", 'start'=>$start, 'end'=>$end, 'items'=>$items));
 	}
 
 	/**
-	 * Show all items due to expire in the current week(starting Monday)
-	 *
-	 * @param  int  $id
-	 * @return \Illuminate\Http\Response
-	 */
-	public function current(Request $request)
-	{
-		//Calculate start and end date that we want
-		$start = Carbon::now()->startOfWeek(Carbon::MONDAY);
-		//Copy is used because using a method updates the object itself ($start==$end otherwise)
-		$end  = $start->copy()->addWeek(1);
-
-		$items = $this->getItems($start, $end);
-
-		return view('reports.expired', array('pageTitle'=>"Expiring this week", 'start'=>$start, 'end'=>$end, 'items'=>$items));
-	}
-
-	/**
-	 * Show all items due to expire in the next week(7 days)
+	 * Show all items due to expire in the current week
 	 *
 	 * @param  int  $id
 	 * @return \Illuminate\Http\Response
@@ -58,13 +45,13 @@ class ReportController extends Controller
 	public function week(Request $request)
 	{
 		//Calculate start and end date that we want
-		$start = Carbon::now();
+		$start = Carbon::now()->startOfWeek();
 		//Copy is used because using a method updates the object itself ($start==$end otherwise)
 		$end  = $start->copy()->addWeek(1);
 
 		$items = $this->getItems($start, $end);
 
-		return view('reports.expired', array('pageTitle'=>"Expiring within next week", 'start'=>$start, 'end'=>$end, 'items'=>$items));
+		return view('reports.expired', array('pageTitle'=>"Expiring this week", 'start'=>$start, 'end'=>$end, 'items'=>$items));
 	}
 
 	/**
@@ -76,6 +63,7 @@ class ReportController extends Controller
 			->whereDate('expiry_date', '<', $end)
 			;})
 			->with('expiry')->get();
+
 		return $items;
 	}
 }
